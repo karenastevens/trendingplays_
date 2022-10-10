84339e9eba8014991bfc335a3c90541d70373b6f
Karena Stevens <kaymark09@gmail.com>
2022-10-10 05:22:19 -0400

    configured app to prevent cached responses

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,5 @@ app = Flask(__name__)
 from app import routes
 app.config.update({'SECRET_KEY': os.environ.get('SECRET_KEY')})
 app.register_blueprint(routes.bp)
+# prevent cached response
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
